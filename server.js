03bd3ef556a20a6514c3387886aa0022a5ae0cba
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-02-13 11:57:15 -0800

    tunely setup

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+app.use(express.static('public'));
+var db = require("./models");
+var controllers = require('./controllers');
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server is up and running on http://localhost:3000");
+});
